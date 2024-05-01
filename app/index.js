2c8ed6837149168d23b1cbf289fe0acaf7d1775a
ArrzGeraldy <Ucihas920@gmail.com>
2024-05-01 22:14:35 +0700

    cors

--- app/index.js
+++ app/index.js
@@ -19,7 +19,12 @@ const app = express();
 const PORT = 4000;
 
 // middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://fooder-eoxseglv1-arrzgeraldys-projects.vercel.app",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
